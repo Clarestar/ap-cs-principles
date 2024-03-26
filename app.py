d4c03000e215723fa6c418c4dcb3f4fc1bf89b07
clarestar <bernardhilary507@gmail.com>
2024-03-26 11:51:55 -0400

    Last Project

--- app.py
+++ app.py
@@ -13,33 +13,26 @@ if __name__ == '__main__':
 import random 
 
 def number_guessing_game():
-    # Generate a random number between 1 and 100
     secret_number = random.randint(1,100)
-
-    # Set the number of attempts
+    secret_number = (61)
+    
     max_attempts = 5
     attempts = 0 
 
-    # Game loop
     while attempts < max_attempts:
-         # Prompt the user to guess the number
         user_guess = int(input("Guess the number (between 1 and 100):"))
 
-        # Check if the guess is correct
-        if user_guess == 61:
+        if user_guess == secret_number:
             print("Congratulations! You guessed the right number.")
         else:
-            if user_guess < 61:
+            if user_guess < secret_number:
                 print("Please try again, the number is higher than the correct answer.")
             else:
                 print("Please try again, the number is lower than the answer.")
             
-        # Increment the attempts
         attempts += 1
 
-    # Inform the user if they couldn't guess the number in the allowed attempts
     if attempts == max_attempts:
-        print(f"Sorry, you've run out of attempts. The correct number was {61}.")
+        print(f"Sorry, you've run out of attempts. The correct number was {secret_number}.")
 
-# Run the game
 number_guessing_game()
--- hello.py
+++ hello.py
@@ -1,19 +1,31 @@
 import random 
 
+number = random.randint(1,100)
+guess = 0
+
+while guess != number:
+    guess = int(input("Enter Guess Number: "))
+    if (guess < number):
+        print("Please try again, the number is lower than the correct answer")
+    elif (guess > number):
+        print ("Please try again, the number is higher than the correct answer")
+    else:
+        print ("Congratulations! You get the correct answer")
+
+
+
+
+import random 
+
 def number_guessing_game():
-    # Generate a random number between 1 and 100
     secret_number = random.randint(1,100)
 
-    # Set the number of attempts
     max_attempts = 5
     attempts = 0 
 
-    # Game loop
     while attempts < max_attempts:
-         # Prompt the user to guess the number
         user_guess = int(input("Guess the number (between 1 and 100):"))
 
-        # Check if the guess is correct
         if user_guess == secret_number:
             print("Congratulations! You guessed the right number.")
         else:
@@ -21,4 +33,11 @@ def number_guessing_game():
                 print("Please try again, the number is higher than the correct answer.")
             else:
                 print("Please try again, the number is lower than the answer.")
-                
+            
+        attempts += 1
+
+    if attempts == max_attempts:
+        print(f"Sorry, you've run out of attempts. The correct number was {61}.")
+
+number_guessing_game()
+    
