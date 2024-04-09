e67e5a5de26d1a5ce15e8f1047ec72aff905e212
clarestar <bernardhilary507@gmail.com>
2024-04-09 09:41:27 -0400

    AP project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import random
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -6,32 +7,42 @@ app = Flask(__name__)
 def hello_world():
     return 'Welcome! to the number-guessing game.'
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=7071)
-
-import random 
-
+@app.route('/guess', methods = ['GET', 'POST'])
 def number_guessing_game():
-    secret_number = random.randint(1,100)
-    secret_number = (61)
+    secret_number = random.randint(1, 100)
+    print('secret number: ' + str(secret_number))
     
     max_attempts = 5
     attempts = 0 
 
-    while attempts < max_attempts:
-        user_guess = int(input("Guess the number (between 1 and 100):"))
-
-        if user_guess == secret_number:
-            print("Congratulations! You guessed the right number.")
-        else:
-            if user_guess < secret_number:
-                print("Please try again, the correct number is higher than this number.")
-            else:
-                print("Please try again, the correct number is lower than this number.")
-            
-        attempts += 1
-
-    if attempts == max_attempts:
-        print(f"Sorry, you've run out of attempts. The correct number was {secret_number}.")
+    if request.method == 'GET':
+        return render_template('guess.html', response = '')
     
-number_guessing_game()
+    if request.method == 'POST':
+        guess = request.form['text']
+        print(guess)
+
+        if int(guess) < secret_number:
+            return render_template('guess.html', response = 'your guess is too low!')
+        else: 
+            return render_template('guess.html', response = 'your guess is too high!')
+    
+
+    # while attempts < max_attempts:
+    #     user_guess = int(input("Guess the number (between 1 and 100):"))
+
+    #     if user_guess == secret_number:
+    #         print("Congratulations! You guessed the right number.")
+    #     else:
+    #         if user_guess < secret_number:
+    #             print("Please try again, the correct number is higher than this number.")
+    #         else:
+    #             print("Please try again, the correct number is lower than this number.")
+            
+    #     attempts += 1
+
+    # if attempts == max_attempts:
+    #     print(f"Sorry, you've run out of attempts. The correct number was {secret_number}.")
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=7071)
--- hello.py
+++ hello.py
@@ -1,43 +1,43 @@
-import random 
+# import random 
 
-number = random.randint(1,100)
-guess = 0
+# number = random.randint(1,100)
+# guess = 0
 
-while guess != number:
-    guess = int(input("Enter Guess Number: "))
-    if (guess < number):
-        print("Please try again, the number is lower than the correct answer")
-    elif (guess > number):
-        print ("Please try again, the number is higher than the correct answer")
-    else:
-        print ("Congratulations! You get the correct answer")
-
-
+# while guess != number:
+#     guess = int(input("Enter Guess Number: "))
+#     if (guess < number):
+#         print("Please try again, the number is lower than the correct answer")
+#     elif (guess > number):
+#         print ("Please try again, the number is higher than the correct answer")
+#     else:
+#         print ("Congratulations! You get the correct answer")
 
 
-import random 
 
-def number_guessing_game():
-    secret_number = random.randint(1,100)
 
-    max_attempts = 5
-    attempts = 0 
+# import random 
 
-    while attempts < max_attempts:
-        user_guess = int(input("Guess the number (between 1 and 100):"))
+# def number_guessing_game():
+#     secret_number = random.randint(1,100)
 
-        if user_guess == secret_number:
-            print("Congratulations! You guessed the right number.")
-        else:
-            if user_guess < secret_number:
-                print("Please try again, the number is higher than the correct answer.")
-            else:
-                print("Please try again, the number is lower than the answer.")
+#     max_attempts = 5
+#     attempts = 0 
+
+#     while attempts < max_attempts:
+#         user_guess = int(input("Guess the number (between 1 and 100):"))
+
+#         if user_guess == secret_number:
+#             print("Congratulations! You guessed the right number.")
+#         else:
+#             if user_guess < secret_number:
+#                 print("Please try again, the number is higher than the correct answer.")
+#             else:
+#                 print("Please try again, the number is lower than the answer.")
             
-        attempts += 1
+#         attempts += 1
 
-    if attempts == max_attempts:
-        print(f"Sorry, you've run out of attempts. The correct number was {61}.")
+#     if attempts == max_attempts:
+#         print(f"Sorry, you've run out of attempts. The correct number was {61}.")
 
-number_guessing_game()
+# number_guessing_game()
     
