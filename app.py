923cb5ab76bcd3ea23b9432deb6c2e230a6cae09
clarestar <bernardhilary507@gmail.com>
2024-04-16 14:36:39 -0400

    Project

--- app.py
+++ app.py
@@ -4,6 +4,22 @@ import random
 app = Flask(__name__)
 
 secret_number = random.randint(1, 100)
+secret_numbers = []
+
+def guess_number_list(guess: int, secret_numbers: list[int]) -> str:
+    for number in secret_numbers:
+        if number == guess:
+            return str(guess) + ' is a secret number, you win!'
+    return str(guess) + ' is not a secret number, you lose!'
+
+def create_secret_numbers():
+    global secret_numbers
+    secret_numbers = []
+    for i in range(10):
+        secret_numbers.append(random.randint(1, 100))
+
+# generate first set of secret numbers
+create_secret_numbers()
 
 @app.route('/')
 def hello_world():
@@ -32,6 +48,18 @@ def number_guessing_game():
         else:
             response = 'Your guess is too high, Please try again!'
         return render_template('guess.html', response = response)
+    
+@app.route('/secret_numbers', methods = ['GET', 'POST'])
+def guess_secret_numbers():
+    if request.method == 'GET':
+        print('secret numbers: ' + str(secret_numbers))
+        return render_template('guess.html', response='')
+    
+    if request.method == 'POST':
+        guess = int(request.form['text'])
+        return guess_number_list(guess, secret_numbers)
+        
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=7071)
